996dd2c3667dc67257016f620550b07b90980ff6
Abbas Hussain <ahussain@elevondata.com>
2025-08-22 01:36:45 +0530

    Add launcher icons and CoordinatorLayout dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 }
